c4f1106725f9ff08277c7f03fd93b69c3babc145
Cameron Prybol <cjprybol@users.noreply.github.com>
2016-08-08 17:44:15 -0700

    dropped singularity exec prepension

--- reproduce_project.sh
+++ reproduce_project.sh
@@ -1,5 +1,5 @@
-singularity exec demo.img wget --no-check-certificate https://stanfordmedicine.box.com/shared/static/qf02pfpv1e87sxz2g1yssufnpe0szbo3.gz -O reads_1.fq.gz
-singularity exec demo.img wget --no-check-certificate https://stanfordmedicine.box.com/shared/static/xu1snsf8st8sbx3x1cejc2f4ltkn0qsc.gz -O reads_2.fq.gz
-singularity exec demo.img wget http://bio.math.berkeley.edu/kallisto/transcriptomes/Saccharomyces_cerevisiae.R64-1-1.rel81.cdna.all.fa.gz
-singularity exec demo.img kallisto index --make-unique --index=index Saccharomyces_cerevisiae.R64-1-1.rel81.cdna.all.fa.gz
-singularity exec demo.img kallisto quant --index=index --output-dir=Kallisto --threads=1 --plaintext reads_1.fq.gz reads_2.fq.gz
+wget --no-check-certificate https://stanfordmedicine.box.com/shared/static/qf02pfpv1e87sxz2g1yssufnpe0szbo3.gz -O reads_1.fq.gz
+wget --no-check-certificate https://stanfordmedicine.box.com/shared/static/xu1snsf8st8sbx3x1cejc2f4ltkn0qsc.gz -O reads_2.fq.gz
+wget http://bio.math.berkeley.edu/kallisto/transcriptomes/Saccharomyces_cerevisiae.R64-1-1.rel81.cdna.all.fa.gz
+kallisto index --make-unique --index=index Saccharomyces_cerevisiae.R64-1-1.rel81.cdna.all.fa.gz
+kallisto quant --index=index --output-dir=Kallisto --threads=1 --plaintext reads_1.fq.gz reads_2.fq.gz
